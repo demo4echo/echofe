6cc24d02312f7f9b26f03204bf839fe200d69bb1
tmeltser <41899083+tmeltser@users.noreply.github.com>
2019-02-14 19:15:02 +0200

    Another comment

--- src/test/java/test/com/efrat/example/devops/echoServerFEApp/resources/echo/EchoTest.java
+++ src/test/java/test/com/efrat/example/devops/echoServerFEApp/resources/echo/EchoTest.java
@@ -23,7 +23,7 @@ import com.efrat.example.devops.echoServerFEApp.resources.echo.EchoResource;
  */
 public class EchoTest
 {
-	// Need to take this externally
+	// Need to take this externally to a file
 	public static final String BASE_URI = "http://192.168.99.100:9999";
 
 	/**
