60b0a35a2691507f117ed0e76146e8d5702cb7b0
root <admin@example.com>
2020-01-08 18:54:22 +0200

    Update

--- launchBuilder.cmd
+++ launchBuilder.cmd
@@ -1,3 +1,9 @@
 @echo off
 
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v "%cd%":/root/repo -w /root/repo demo4echo/alpine_openjdk8_k8scdk:latest ash
+REM Minikube formatr (needs to add some manipulation on the path:
+REM If the path to mounted (local) is => C:\Users\tmeltse.COMVERSE\OneDrive - Mavenir Ltd\GitClones\GitHub\eclipse
+REM Then the manipulated path would be => /c/Users/tmeltse.COMVERSE/OneDrive - Mavenir Ltd/GitClones/GitHub/eclipse (add "/" in the beginning ; replace "C" with "c" ; remove ":" ; "replace "\" with "/")
+REM So the updated command would be => docker run -it --rm -v "/c/Users/tmeltse.COMVERSE/OneDrive - Mavenir Ltd/GitClones/GitHub/eclipse":$HOME/repo alpine ash
+
+REM Docker Desktop format (no manipulation is needed), but the C drive needs to be shared once through the Docker Desktop GUI!
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v %userprofile%\.gradle:/$HOME/.gradle -v %userprofile%\.helm:/$HOME/.helm -v "%cd%":$HOME/repo -w $HOME/repo demo4echo/alpine_openjdk8_k8scdk:latest ash
