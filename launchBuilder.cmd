e10200084917b118caf159c2b689ebb8666b8f52
root <admin@example.com>
2020-01-08 21:48:28 +0200

    Update

--- launchBuilder.cmd
+++ launchBuilder.cmd
@@ -6,4 +6,4 @@ REM Then the manipulated path would be => /c/Users/tmeltse.COMVERSE/OneDrive - M
 REM So the updated command would be => docker run -it --rm -v "/c/Users/tmeltse.COMVERSE/OneDrive - Mavenir Ltd/GitClones/GitHub/eclipse":$HOME/repo alpine ash
 
 REM Docker Desktop format (no manipulation is needed), but the C drive needs to be shared once through the Docker Desktop GUI!
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v %userprofile%\.gradle:$HOME/.gradle -v %userprofile%\.helm:$HOME/.helm -v "%cd%":$HOME/repo -w $HOME/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' demo4echo/alpine_openjdk8_k8scdk:latest ash
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v %userprofile%\.gradle:/root/.gradle -v %userprofile%\.helm:/root/.helm -v "%cd%":/root/repo -w /root/repo -e MARK_OFF_CLUSTER_INVOCATION_ENV_VAR='dc' demo4echo/alpine_openjdk8_k8scdk:latest ash
