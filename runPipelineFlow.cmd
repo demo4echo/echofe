149f80283956a0d64dd12a0fce985349a1d2cb9f
root <admin@example.com>
2020-01-14 19:51:53 +0200

    Update

--- runPipelineFlow.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set MARK_OFF_CLUSTER_INVOCATION_ENV_VAR="dc"
-
-call gradlew.bat %* AIO
--- runPipelineFlow.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-export MARK_OFF_CLUSTER_INVOCATION_ENV_VAR="dc"
-
-./gradlew $# AIO
